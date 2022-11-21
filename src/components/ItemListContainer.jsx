e2a2995bcfad4001941ec2e2961a13c1c06c76b2
andrea sanso  alises <asansoalises@gmail.com>
2022-11-21 10:35:08 -0300

    v9

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 import { gFetch } from "../utils/gFetch";
 import ItemList from "./ItemList";
@@ -7,13 +8,21 @@ import ItemList from "./ItemList";
 const ItemListContainer = (obj)=> {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
-
+const {categoriaId}=useParams()
   useEffect(()=>{
-  gFetch("1")
- .then(resp => setProducts(resp))
- .catch (err=> console.log(err))
- .finally(()=>setLoading(false))
-},[])
+    if (categoriaId) {
+      gFetch()
+      .then(resp => setProducts(resp.filter(prod=>prod.categoria===categoriaId)))
+      .catch (err=> console.log(err))
+      .finally(()=>setLoading(false))
+    }else{
+      gFetch()
+      .then(resp => setProducts(resp))
+      .catch (err=> console.log(err))
+      .finally(()=>setLoading(false))
+    }
+
+},[categoriaId])
 
   return (
 
